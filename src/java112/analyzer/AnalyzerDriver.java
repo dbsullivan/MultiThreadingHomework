38ce545a3369891c2752a791a023608062bd3f11
Dave Sullivan <dbsullivan@madisoncollege.edu>
2015-11-15 10:19:14 -0600

    initial

--- src/java112/analyzer/AnalyzerDriver.java
+++ src/java112/analyzer/AnalyzerDriver.java
@@ -4,8 +4,12 @@ public class AnalyzerDriver {
 
     public static void main(String[] args) {
         //TODO Add code to write out the time it takes to run
+        System.out.println("Start time: " + System.currentTimeMillis());
+
         AnalyzeFile analyzer = new AnalyzeFile();
         analyzer.runAnalysis(args);
+
+        System.out.println("End time: " + System.currentTimeMillis());
     }
 
 }
